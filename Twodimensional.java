c667e8b7e7f1f5d7981440919e98d661f5f65b35
170030506 <56549398+170030506@users.noreply.github.com>
2020-01-28 16:36:09 +0530

    Add files via upload

---
+++ Twodimensional.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Twodimensional{
+    public static void main(String args[]){
+        for(int i=1;i>=0;i--){
+            for(int j=1;j>=0;j--){
+               System.out.print(Integer.parseInt(args[i][j]) );
+            }
+        }
+    }
+}
